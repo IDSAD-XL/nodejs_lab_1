1a6b1741106eda6056f914765cdf566945551d87
IDSAD <idsad.ch@gmail.com>
2022-11-20 21:57:26 +0200

    changed: fixed functions deployment and some configs. reason: environment setup.

--- functions/hello.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {Request} req
- * @param {Response} res
- * @return {Promise}
- * */
-export async function hello(req, res) {
-  return res.send("Hello!");
-}
--- index.js
+++
@@ -1,7 +0,0 @@
-const a = 15;
-
-const checkFunction = (check) => {
-  return check > 10;
-};
-
-checkFunction(a);
